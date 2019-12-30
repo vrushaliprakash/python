f1f0be7f42fe5c7cc44ae7781833eab3381c1895
vrushaliprakash <59351989+vrushaliprakash@users.noreply.github.com>
2019-12-30 12:32:49 +0530

    if statement

---
+++ Part 7 - if statement.py
@@ -0,0 +1,25 @@
+
+x = 2
+y = 7
+z = 10
+
+if x > y:
+    print(x,'is greater than',y)
+if x < y:
+    print(x,'is less than',y)
+if x == y:
+    print(x,'is the same as',y)
+    
+# cannot do this:
+#if x < '2':
+#    print(x,'is less than 2')
+
+if x <= y:
+    print(x,'is less than or equal to',y)
+
+if z > y > x:
+    print(z,'is greater than',y,'which is greater than',x)
+
+
+
+    
